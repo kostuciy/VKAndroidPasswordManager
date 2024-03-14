16467f3b6cf68924b6a340b1e7b9c35328383b3d
Kot512 <kelohov512@gmail.com>
2024-03-14 11:11:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vkandroidpasswordmanager/view/fragments/ListFragment.kt
@@ -0,0 +1,23 @@
+package com.example.vkandroidpasswordmanager.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.vkandroidpasswordmanager.R
+import com.example.vkandroidpasswordmanager.databinding.FragmentListBinding
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentListBinding.inflate(inflater, container, false)
+
+        //
+
+        return binding.root
+    }
+}
